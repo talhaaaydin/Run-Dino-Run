e80b27a976e1c282af1e8163b54e1a4df0a0725a
talhaaaydin <40953163+talhaaaydin@users.noreply.github.com>
2019-02-26 03:27:13 +0300

    Last Update
    Bu son güncellemeyle birlikte küçük bir credits ekranı var. Yeni bir icon oluşturuldu. Amazon AppStore'a yüklendi.

---
+++ Run Dino Run/Assets/IconGenerator/IconGenerator.cs
@@ -0,0 +1,65 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class IconGenerator : MonoBehaviour {
+	
+	[Header("Prefabs")]
+	public GameObject[] targets;
+	public string[] fileNames;
+	[Header("Custom settings")]
+	[Tooltip("Custom folder used to save the icon. LEAVE EMPTY FOR DEFAULT!")]
+	public string customFolder = ""; // OOPS! You need to create folder manually, before running the script!
+	public bool usePrefabNames = false;
+
+	[Header("Icons")]
+	public Texture rawIcon;
+	public Texture2D icon;
+
+	void Start ()
+	{
+		int targetCount = 0;
+		string iconName;
+
+		if (targets == null) // Check if targets are specified
+		{
+			Debug.LogError ("You need to specify targets!");
+			return;
+		}
+
+		foreach (GameObject target in targets)
+		{
+			rawIcon = AssetPreview.GetAssetPreview (target);
+			icon = rawIcon as Texture2D;
+
+			byte[] bytes = icon.EncodeToPNG ();
+
+			if(usePrefabNames) // Check if usePrefabNames is enabled!
+				iconName = target.name;
+			else
+				iconName = fileNames [targetCount];
+
+			GameObject.Find("Canvas").GetComponent<IconGeneratorUIExample>().AddImage (icon,iconName); // Used for example, can be removed!
+
+			if (customFolder == "") // Check if custom folder is specified!
+			{
+				File.WriteAllBytes (Application.dataPath + "/" + iconName + ".png", bytes);
+				Debug.Log ("File saved in: " + Application.dataPath + "/" + iconName + ".png");
+			}
+			else
+			{
+				File.WriteAllBytes (Application.dataPath + "/" + customFolder + "/" + iconName + ".png", bytes);
+				Debug.Log ("File saved in: " + Application.dataPath + "/" + customFolder + "/" + iconName + ".png");
+			}
+
+			targetCount++;
+		}
+	}
+}
+
+#endif
---
+++ Run Dino Run/Assets/IconGenerator/Readme.txt
@@ -0,0 +1,18 @@
+Hello, thank you for downloading my asset! If you like it, please consider rating it on the asset store!
+If you have any questions / problems, please contact me at: rockis167@gmail.com
+
+-MrRockis
+
+
+FAQ:
+Q: How to use this asset?
+A: Please check HowToUse.txt !
+
+Q: I get "IndexOutOfRangeException" - error!
+A: You forgot to specify the file name in the "File Names" section, or did not enable "Use Prefab Names" option!
+
+Q: No icons were generated...
+A: Did you run the scene? Also currently unity has a bug that you need to click outside the editor window, for the icons to be refreshed.
+
+A: Is it fully free?
+Q: YES!
